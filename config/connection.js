9ed679c1bff1e5aa362d7be6ab3508018358372d
Arpita Desai <patelarpita29@gmail.com>
2020-06-05 19:41:59 -0700

    directory structure created

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burger_db"
+});
+
+connection.connect(err => {
+    if(err) {
+        console.error("Connection error: " + err.stack);
+        return;
+    }
+    console.log("Connected as Id " + connection.threadId);
+});
+
+
+module.exports = connection;
---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+insert into burgers (burger_name, devoured)
+values 
+('Cheeseburger Deluxe', false), 
+('Cheeseburger', false), 
+('Hamburger', false),
+('Vegan Burger', false);
