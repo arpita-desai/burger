b2799af41da861a03d87777acab930e882aa09ec
Arpita Desai <patelarpita29@gmail.com>
2020-06-06 18:31:57 -0700

    did changes for Heeroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@ const mysql = require("mysql");
 
 let connection;
 
-connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burger_db"
-});
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "burger_db"
+    });
+}
 
 connection.connect(err => {
     if(err) {
